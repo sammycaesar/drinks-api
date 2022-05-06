b75ba6a7d386313f815c6c30c4c335f86ac60fbd
Samantha Caesar <samantha.caesar@bigpond.com>
2022-05-06 12:11:01 +0200

    intial REST API for cafe drinks!

--- backend/controllers/drinkController.js
+++ backend/controllers/drinkController.js
@@ -1,22 +1,28 @@
 // Sync to Mongoose
 const asyncHandler = require("express-async-handler");
 
+const Drink = require("../models/goalModel");
+
 // desc:    Get drinks
 // route:   GET /api/drinks
 // access:  Private
 const getDrinks = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
-    res.status(400);
-    throw new Error("pls add a text field");
-  }
-
-  res.status(200).json({ message: "Get drinks" });
+  const drinks = await Drink.find();
+  res.status(200).json(drinks);
 });
 
 // desc:    Set drinks
 // route:   POST /api/drinks
 // access:  Private
 const setDrink = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("pls add a text field");
+  }
+
+  const drink = await Drink.create({
+    text: req.body.text,
+  });
   res.status(200).json({ message: "Set drinks" });
 });
 
@@ -24,14 +30,34 @@ const setDrink = asyncHandler(async (req, res) => {
 // route:   UPDATE /api/drinks
 // access:  Private
 const updateDrink = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update drink ${req.params.id}` });
+  const drink = await Drink.findbyId(req.params.id);
+
+  if (!drink) {
+    res.status(400);
+    throw new Error("Drink not found");
+  }
+
+  const updatedDrink = await Drink.findbyIdandUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedDrink);
 });
 
 // desc:    Delete drinks
 // route:   DELETE /api/drinks
 // access:  Private
 const deleteDrink = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete drink ${req.params.id}` });
+  const drink = await Drink.findbyId(req.params.id);
+
+  if (!drink) {
+    res.status(400);
+    throw new Error("Drink not found");
+  }
+
+  await drink.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 // Export module
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 //Import and activate package
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
